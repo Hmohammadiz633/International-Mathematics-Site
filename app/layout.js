0496b079453e88e0b5a2b2e051c02ade0e3e5ab9
Hmohammadiz633 <hadi.mohammadi.zarandini@gmail.com>
2026-08-06 00:00:47 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,23 @@
-// app/layout.js
 import './globals.css';
 
 export const metadata = {
-  title: 'آموزش بین‌المللی ریاضیات | International Math',
-  description: 'سایت تخصصی آموزش ریاضیات بین‌الملل',
+  title: 'آموزش بین‌المللی ریاضی و تدریس خصوصی GCSE Maths',
+  description: 'سایت تخصصی آموزش بین‌المللی ریاضی، تدریس خصوصی ریاضی و دوره‌های تخصصی GCSE Maths با بالاترین کیفیت.',
+  keywords: [
+    'آموزش بین المللی ریاضی',
+    'GCSE Maths',
+    'تدریس خصوصی ریاضی',
+    'ریاضی بین‌الملل'
+  ],
+  authors: [{ name: 'International Math' }],
+  openGraph: {
+    title: 'آموزش بین‌المللی ریاضی و تدریس خصوصی GCSE Maths',
+    description: 'سایت تخصصی آموزش بین‌المللی ریاضی، تدریس خصوصی ریاضی و دوره‌های تخصصی GCSE Maths.',
+    url: 'https://internationalmaths.ir',
+    siteName: 'International Math',
+    locale: 'fa_IR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
